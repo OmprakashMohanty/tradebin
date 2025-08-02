db9454b4aa7282a9424787a4b2fb0fe823f76b52
OmprakashMohanty <omprakashmohanty782@gmail.com>
2025-08-02 20:51:01 +0530

    Initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-red-400 h-screen items-center flex  justify-center">
+      <span className="font-bold tracking-wider text-5xl">hello</span>
+    </div>
+  )
+}
